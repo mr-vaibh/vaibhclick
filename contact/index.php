7850484192ad73ac84b35c81d2374fb3747c4c58
Vaibhav Shukla <52421094+mr-vaibh@users.noreply.github.com>
2021-05-07 20:33:41 +0530

    Update index.php

--- contact/index.php
+++ contact/index.php
@@ -1,19 +1,93 @@
+<?php
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$msg=$_POST['msg'];
 
+		$to='shuklavaibhav336@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form Submission';
+		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Message Sent Successfully! Thank you"." ".$name.", We will contact you shortly! (with LOVE💓)</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Feedback Form</title>
-	<style type="text/css" media="screen">
+  <title>Feedback | VaiBHClick</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body background="..\images\site_home_background.jpg">
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#" style="font-size:30px">VaiBHClick</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.html">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Lessons<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="\website projects\vaibhclick.com\programming.html">Programming</a></li>
+          <li><a href="#">Windows Tricks</a></li>
+		  <li><a href="#">Ethical/White Hat Hacking</a></li>
+          <li><a href="#">YouTube Channel</a></li>
+        </ul>
+      </li>
+	  
+	  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Download<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="game-download.php">Games</a></li>
+          <li><a href="movie-download.php">Movies</a></li>
+          <li><a href="dll-download.php">DLL-Files</a></li>
+		  <li><a href="#">Operrating System (.iso)</a></li>
+		  <li><a href="#">Projects</a></li>
+        </ul>
+      </li>
+      <li><a href="contact/index.html">Contact</a></li>
+	  <li><a href="about.html">About Me</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+	<form class="navbar-form navbar-left" action="/action_page.php">
+  <div class="input-group">
+    <input type="text" class="form-control" placeholder="Search">
+    <div class="input-group-btn">
+      <button class="btn btn-default" type="submit">
+        <i class="glyphicon glyphicon-search"></i>
+      </button>
+    </div>
+  </div>
+</form>
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+   <style type="text/css" media="screen">
 		*{
 			margin: 0;
 			padding: 0;
 			box-sizing: border-box;
 			font-family: helvetica;
 		}
-	     
+
 		.main{
 			width: 500px;
-			margin: 50px auto;
+			margin: 20px auto;
 			border-radius: 10px;
 			border: 5px solid #dc143c;
 			border-left: 40px solid #dc143c;
@@ -83,11 +157,10 @@
 			}
 		}
 	</style>
-</head>
-<body background="images\site_home_background.jpg">
+
 	<div class="main">
 		<div class="info">Give Your Feedback!</div>
-		<form method="post" name="form" class="form-box">
+		<form action="mail_handler.php" method="post" name="form" class="form-box">
 			<label for="name">Name</label><br>
 			<input type="text" name="name" class="inp" placeholder="Enter Your Name" required><br>
 			<label for="email">Email ID</label><br>
@@ -99,25 +172,7 @@
 			<input type="submit" name="submit" value="Send" class="sub-btn">
 		</form>
 	</div>
+</div>
+
 </body>
-
-<?php
-	if(isset($_POST['submit'])){
-		$name=$_POST['name'];
-		$email=$_POST['email'];
-		$phone=$_POST['phone'];
-		$msg=$_POST['msg'];
-
-		$to='shuklavaibhav336@gmail.com'; // Receiver Email ID, Replace with your email ID
-		$subject='Form Submission';
-		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
-		$headers="From: ".$email;
-
-		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Message Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-		}
-		else{
-			echo "Something went wrong!";
-		}
-	}
-?>
+</html>
